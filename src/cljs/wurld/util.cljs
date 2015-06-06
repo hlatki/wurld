6378d57fdccc74ac1746b22e4495fa5a890baa0f
hlatki <hannahatkinson@gmail.com>
2015-06-06 15:58:12 -0700

    Add functions to check words for TLDS and return domain names
    Also hardcode set of TLDS (from
    http://data.iana.org/TLD/tlds-alpha-by-domain.txt version 2015060501).

---
+++ src/cljs/wurld/util.cljs
@@ -0,0 +1,29 @@
+(ns wurld.util
+  (:require wurld.tlds))
+
+
+(defn build-url
+  "Create URL by inserting . between name and TLD, given location of split"
+  [s i]
+  (str (subs s 0 i) "." (subs s i)))
+
+(defn check-tld
+  "Given string and position pos, look up (subs s p) in tld set and return url.
+  Otherwise return false"
+  [s pos]
+  (let [tld (subs s pos)]
+    (if (contains? wurld.tlds/tld-set tld)
+      (build-url s pos)
+      false)))
+
+;;TODO: clean this function up a little
+(defn get-possible-urls
+  "Get possible urls from word"
+  [word]
+  (loop [pos (dec (count word))
+         acc []]
+    (if (<= pos 0)
+      acc
+      (if-let [domain-name (check-tld word pos)]
+        (recur (dec pos) (conj acc domain-name))
+        (recur (dec pos) acc)))))
