faa7a8a46e3bf566d207b2f4946a03c4471f7fcc
hlatki <hannahatkinson@gmail.com>
2015-06-07 12:36:34 -0700

    Delete compiled output

--- resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [wurld.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:websocket-url "ws://localhost:3449/figwheel-ws", :on-jsload (fn [& x] (if js/wurld.core.main (apply js/wurld.core.main x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'wurld.core/main' is missing"))), :build-id "dev"})
-
