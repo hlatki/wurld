e20cd63e8729f923f56c0632e04c0d1dd945f0be
hlatki <hannahatkinson@gmail.com>
2015-07-04 16:58:09 -0700

    Start moving stuff into cljs

--- src/cljs/wurld/core.cljs
+++ src/cljs/wurld/core.cljs
@@ -3,6 +3,9 @@
             [wurld.util :refer [get-possible-urls]]))
 
 
+(def title-text "Hello, Wurld!")
+(def sub-title-text "Because .com's are for your grandpa, brah")
+(def marketing-nonesense "Wurld helps you figure out what kind of cool single word URL's you can make from your #brand. For instance, say you're Nautical Inc. Wurld will tell you that your new home could be nautic.al or nauti.cal like you're a hot new startup and not a 150 year old purveyor of sailcloth.")
 
 
 ;; This handy funtcion is apdapted from the Reagent website (https://reagent-project.github.io/)
@@ -27,6 +30,10 @@
   (let [value (atom "clojure")]
     (fn []
       [:div
+       {:class "jumbotron"}
+       [:h1 title-text]
+       [:h2 sub-title-text]
+       [:p marketing-nonesense]
         [:p "Enter a word: " [input-component value]]
         [:div "Possible URLs:" [lister (get-possible-urls @value)]]])))
 
